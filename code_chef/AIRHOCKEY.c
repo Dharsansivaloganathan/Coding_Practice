44db4e6601d66e3812fa97168649955ae77f9523
Dharsan Sivaloganathan <98682637+Dharsansivaloganathan@users.noreply.github.com>
2022-11-17 00:21:37 +0530

    Create AIRHOCKEY.c

---
+++ code_chef/AIRHOCKEY.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	int t,x,y,max,p;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    scanf("%d",&x);
+	    scanf("%d",&y);
+	    if(x>y)
+	    {
+	        max=x;
+	    }
+	    else
+	    {
+	        max=y;
+	    }
+	    p=7-max;
+	    printf("%d\n",p);
+	}
+	return 0;
+}
