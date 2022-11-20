b96021902601fbc40275c0a3e7daae932aa1e1a9
Dharsan Sivaloganathan <98682637+Dharsansivaloganathan@users.noreply.github.com>
2022-11-20 23:39:57 +0530

    Create Parity.c

---
+++ code_chef/Parity.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,n;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    scanf("%d",&n);
+	    if(n%2==0)
+	    {
+	        printf("YES\n");
+	    }
+	    else
+	    {
+	         printf("NO\n");
+	    }
+	}
+	return 0;
+}
+
