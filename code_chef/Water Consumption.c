96ae8a6e189eaaebaeec38262b020ade141bd0aa
Dharsan Sivaloganathan <98682637+Dharsansivaloganathan@users.noreply.github.com>
2022-11-23 00:44:03 +0530

    Create Water Consumption.c

---
+++ code_chef/Water Consumption.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+	// your code goes here
+	int main(void)
+	
+{	int x,T,i;
+    scanf("%d",&T);
+	for(i=0;i<T;i++)
+	{
+	    scanf("%d",&x);
+	 if(x>=2000)
+	 {
+	  printf("YES\n");
+	 
+	 }
+	 else{
+	     printf("NO\n");
+	 }
+	}
+	return 0;
+}
