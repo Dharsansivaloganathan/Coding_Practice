18442da50d9bb9254a2b7471e827a634d5100ba7
Dharsan Sivaloganathan <98682637+Dharsansivaloganathan@users.noreply.github.com>
2022-11-23 00:11:56 +0530

    Create Weights.c

---
+++ code_chef/Weights.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int w,x,y,z;
+	    scanf("%d %d %d %d",&w,&x,&y,&z);
+	    if((x+y)==w || (y+z)==w || (x+z)==w || x==w || y==w || z==w || (x+y+z)==w){
+	        printf("YES\n");
+	    }
+	    else{
+	        printf("NO\n");
+	    }
+	}
+	return 0;
+}
