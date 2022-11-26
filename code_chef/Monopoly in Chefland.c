62d36307022289994bc5b35cf257f698cbe16584
Dharsan Sivaloganathan <98682637+Dharsansivaloganathan@users.noreply.github.com>
2022-11-27 00:59:59 +0530

    Create Monopoly in Chefland.c

---
+++ code_chef/Monopoly in Chefland.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int test_cases,r1,r2,r3;
+	scanf("%d",&test_cases);
+	while(test_cases--)
+	{
+	    scanf("%d%d%d",&r1,&r2,&r3);
+	    if(r3>r1+r2 || r2>r1+r3 || r1>r2+r3)
+	       printf("Yes\n");
+	    else
+	       printf("No\n");
+	}
+	return 0;
+}
