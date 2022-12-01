db5f8b6be72ec3d0a0a543f54bfc6d9e20712496
Dharsan Sivaloganathan <98682637+Dharsansivaloganathan@users.noreply.github.com>
2022-12-01 22:31:41 +0530

    Create Minimum Number of Ones.cpp

---
+++ code_chef/Minimum Number of Ones.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T, N;
+    cin >> T;
+    while (T--)
+    {
+        cin >> N;
+        cout << (N / 2) << endl;
+    }
+    return 0;
+}
