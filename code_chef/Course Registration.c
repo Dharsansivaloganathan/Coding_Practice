d913121d4501a8bfd426dbd556704308c7705c3b
Dharsan Sivaloganathan <98682637+Dharsansivaloganathan@users.noreply.github.com>
2022-12-09 11:56:12 +0530

    Create Course Registration.c

---
+++ code_chef/Course Registration.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main( )
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+       int x,y,z;
+       scanf("%d %d %d",&x,&y,&z);
+       if((y-z)>=x)
+       printf("yes\n");
+       else
+       printf("no\n");
+    }
+    return 0;
+}
