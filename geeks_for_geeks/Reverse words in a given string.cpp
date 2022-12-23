04c661de834d45372a720d0aa13dc9f1c1c8569f
Dharsan Sivaloganathan <98682637+Dharsansivaloganathan@users.noreply.github.com>
2022-12-23 20:03:30 +0530

    Create Reverse words in a given string.cpp

---
+++ geeks_for_geeks/Reverse words in a given string.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to reverse words in a given string.
+    string reverseWords(string S) 
+    { 
+        // code here 
+        stack <string> words;
+        string a =" ";
+        
+        for(int i=0; S[i] != '\0'; i++)
+        {
+            if (S[i] == '.')
+            {
+                words.push(a);
+                a="";
+                
+            }
+            else
+            {
+                a+= S[i];
+            }
+        }
+        words.push(a);
+        
+        string ans ="";
+        ans+= words.top();
+        words.pop();
+        
+        while(!words.empty())
+        {
+            ans+='.';
+            ans += words.top();
+            words.pop();
+            
+        }
+        return ans; 
+        
+    } 
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+    int t;
+    cin >> t;
+    while (t--) 
+    {
+        string s;
+        cin >> s;
+        Solution obj;
+        cout<<obj.reverseWords(s)<<endl;
+    }
+}
+// } Driver Code Ends
